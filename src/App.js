11e91e459a95d45d75e7c879a11600c19b47a22d
yang vieira <yangflix3@gmail.com>
2022-06-07 02:43:12 -0400

    subindo

--- src/App.js
+++ src/App.js
@@ -4,13 +4,38 @@ function App() {
   return (
     <div>
       <header>
-       <h1>Essa é pra ela</h1>
+        <h1>Essa é pra ela</h1>
       </header>
       <div className='poema'>
-      <h2> Onde tudo começou</h2>
-      <p>Um certo dia as 11:40 da manhã precisava ir no centro renovar meu cartão do onibus</p>
+        <h2> Onde tudo começou</h2>
+        <p>Um certo dia as 11:40 da manhã precisava ir no centro renovar meu cartão do onibus, no
+          ponto avistei <strong>Ela</strong> uma mina do cabelo cacheado parecia um jardim, olhos pequenos
+          e cativante, labios bem desenhados e lindos, simplesmente uma Deusa.
+        </p>
       </div>
-    
+
+      <div className='poema2'>
+        <p>Manooo nem a conhecia não sabia o nome, queria muito conhecer ela, a energia dela
+          era boa de mais, tipo raios de sol de uma manhã fresquinha, começei a procurar pelo facebook
+          e adivinha... depois de uma semana o facebook me mostrou ela nas sugestões, fiquei tão felizzzz.
+        </p>
+      </div>
+
+      <div className='poema2'>
+        <p>Consegui me conectar através de um video que fiz só mostrando a onde a vi pela primeira
+          vez, de certa forma ela gostou muitooo, muitoo mesmoo, ela até guardouuu, ela é extraordinária
+          o simples conquistou o coração dela.
+        </p>
+      </div>
+
+      <div className='poema2'>
+        <h2>O Sentimento</h2>
+        <p>Então hoje só somos amigos e fico muito feliz por isso, será que já fui apaixonado por ela?
+          manoooo quando vi ela pela primeira vez meu coração vibrou, me apaixonei por cada detalhe quando olhava ela meu sentimento não
+          negava, mas emfim fui soldado orgulhoso que chegou no olimpo e conheceu essa <strong>divindade da beleza.</strong>
+        </p>
+      </div>
+
     </div>
   );
 }
